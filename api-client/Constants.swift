1499dc146cf1fe1f9ea31e2e563d8817829065bd
Ka Kin Chiu <aeaopa79191@gmail.com>
2017-01-13 03:08:52 -0800

    DataService is done
    AuthService will need to be done

---
+++ api-client/Constants.swift
@@ -0,0 +1,28 @@
+//
+//  Constants.swift
+//  api-client
+//
+//  Created by KaKin Chiu on 1/12/17.
+//  Copyright © 2017 KaKinChiu. All rights reserved.
+//
+
+import Foundation
+
+//Callbacks
+//Typealias for callbacks used in Data Service
+typealias callback = (_ success:Bool) ->()
+
+//Base URL
+let BASE_API_URL = "http://localhost:3005/api/v1"
+
+//Get all food trucks
+let GET_ALL_FT_URL = "\(BASE_API_URL)/foodtruck"
+
+//Get all reviews for a specific food truck
+let GET_ALL_FT_Reviews = "\(BASE_API_URL)/foodtruck/reviews"
+
+// POST add new Food Truck
+let POST_ADD_NEW_TRUCK = "\(BASE_API_URL)/foodtruck/add"
+
+//POST add review for a specific food truck
+let POST_ADD_NEW_REVIEW = "\(BASE_API_URL)/foodtruck/reviews/add"
