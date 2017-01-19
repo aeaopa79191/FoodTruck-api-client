7c7d37e1222928df9145079ed305f17d99eb2e3b
Ka Kin Chiu <aeaopa79191@gmail.com>
2017-01-18 16:30:48 -0800

    implemented cancel action and alerts

--- api-client/AddTruckVC.swift
+++ api-client/AddTruckVC.swift
@@ -9,18 +9,75 @@
 import UIKit
 
 class AddTruckVC: UIViewController {
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var foodTypeField: UITextField!
+    @IBOutlet weak var avgCostField: UITextField!
+    @IBOutlet weak var latField: UITextField!
+    @IBOutlet weak var longField: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    
+    }
+    
+    
+    @IBAction func addButtonTapped(_ sender: UIButton) {
+        guard let name = nameField.text, nameField.text != ""
+            else{
+                showAlert(with: "Error", message: "Please enter a name.")
+                return
+        }
         
-
+        guard let foodtype = foodTypeField.text, foodTypeField.text != ""
+            else{
+                showAlert(with: "Error", message: "Please enter a food type.")
+                return
+        }
+        
+        guard let avgcost = avgCostField.text, avgCostField.text != ""
+            else{
+                showAlert(with: "Error", message: "Please enter an average cost.")
+                return
+        }
+        
+        guard let latitude = latField.text, latField.text != ""
+            else{
+                showAlert(with: "Error", message: "Please enter a latitude.")
+                return
+        }
+        
+        guard let longitude = longField.text, longField.text != ""
+            else{
+                showAlert(with: "Error", message: "Please enter a longitude.")
+                return
+        }
+        
+    }
+    
+    @IBAction func cancelButtonTapped(_ sender: UIButton) {
+        dissmissViewController()
     }
     
     @IBAction func backButtonTapped(sender: UIButton){
-        _ = navigationController?.popViewController(animated: true)
+        dissmissViewController()
     }
 
+    func dissmissViewController(){
+        OperationQueue.main.addOperation {
+            _ = self.navigationController?.popViewController(animated: true)
+        }
+    }
+    
+    func showAlert(with title:String? ,message: String?){
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
 
 
 }
